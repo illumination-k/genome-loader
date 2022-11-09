056d7191dd04b21a4f08a0618a9a009b1c711a0e
illumination-k <illumination.k.27@gmail.com>
2022-11-09 18:01:23 +0900

    added blast script generation and add config in pyproject.toml

--- genome_loader/main.py
+++ genome_loader/main.py
@@ -1,15 +1,23 @@
+from typing import Literal
+
 import click
-from genome_loader.config import ConfigModel
+
+from genome_loader.logger_utils import set_loglevel, LogLevel
 from genome_loader import handlers
+from genome_loader.config import ConfigModel
+
 
 @click.group()
-def main():
-    pass
+@click.option("--loglevel", type=click.Choice(["debug", "info", "warn", "error"]))
+def main(loglevel: LogLevel = "error"):
+    set_loglevel(loglevel)
+
 
 @main.command("sync")
 def sync():
     config = "genomes.json"
     handlers.sync(ConfigModel.parse_file(config))
+
 
 if __name__ == "__main__":
     main()
